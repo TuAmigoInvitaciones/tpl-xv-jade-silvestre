f3c32fc6afa1d6c31dd6c584b9c004d5a694b7bb
TUAmigoInvitaciones <tuamigoinvitaciones@gmail.com>
2026-08-27 20:24:00 -0600

    feat(title) ✨: Set title document correctly

--- src/InvitationApp.tsx
+++ src/InvitationApp.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -10,16 +10,8 @@ import { DrawerMaster } from '@/common/components/drawer/DrawerMaster'
 import { ToastContainer } from '@/common/components/toast/ToastContainer'
 import { Menu } from '@/common/components/menu/Menu'
 import { MusicPlayer } from '@/common/components/music-player/MusicPlayer'
-import { useInvitationConfig } from '@/common/hooks'
 
 const InvitationAppContent: React.FC = () => {
-  const { sections, theme } = useInvitationConfig()
-
-  useEffect(() => {
-    const coupleNames = sections.hero?.names || theme.menu?.title || 'Invitación Especial'
-    document.title = `${coupleNames} | Invitación Especial`
-  }, [sections, theme])
-
   return (
     <BrowserRouter>
       <RouterApp />
